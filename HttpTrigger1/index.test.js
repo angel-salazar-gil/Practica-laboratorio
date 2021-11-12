7637560b0d8298ab3477631bb502c523afaf3bb8
angel-salazar-gil <59419505+angel-salazar-gil@users.noreply.github.com>
2021-11-11 23:16:06 -0500

    Update index.test.js

--- HttpTrigger1/index.test.js
+++ HttpTrigger1/index.test.js
@@ -1,13 +1,21 @@
-const functions = require('./index');
-const context = require('../testing/Context');
-//const { test } = require('@jest/globals');
+const functions= require('./index');
+const context= require('../testing/Context');
+const { expect } = require('@jest/globals');
 
-test('Http trigger', async () =>{
+test('Http trigger example', async () => {
     const request = {
-        query: { name: 'Angel' }
+        query: { name: 'schuc'}
     };
 
-    await functions(context, request);
-    expect(context.res.body).toEqual('Hello, Angel');
-    expect(context.log.mock.calls.length).toBe(1);
+    var iteration = 100;
+    console.time('FUNCTION #1');
+    for( var i = 0; i<iteration; i++ ) 
+    {
+        await functions(context, request);
+    }
+    console.timeEnd('FUNCTION #1');
+    expect(context.res.body).toContain('W');
+    expect(context.res.body).toEqual('Welcome, schuc');
+    expect(context.log.mock.calls.length).toBe(200);
+
 });
